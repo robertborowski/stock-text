e13779de3844e4decf4b2006d96d1e86dfba0c6f
robert borowski <rborowski141@gmail.com>
2021-02-22 21:26:01 -0500

    test Procfile edit for deployment2

--- backend/db/connect_to_database.py
+++ backend/db/connect_to_database.py
@@ -6,10 +6,17 @@ def connect_to_postgres_function():
   """
   Returns: Postgres Connection and cursor
   """
+  #=============================
+  DATABASE_URL = os.environ.get('DATABASE_URL')
+  connection_postgres = psycopg2.connect(DATABASE_URL, sslmode='require')
+  print('- - - - - - - - - - - - - - CONNECTED WOOHOO!!!!! - - - - - - - - - - - - -')
+  #=============================
+  """
   connection_postgres = psycopg2.connect(user = os.environ.get('POSTGRESQL_LOCAL_USER'),
                                         password = os.environ.get('POSTGRESQL_LOCAL_PASSWORD'),
                                         host = os.environ.get('POSTGRESQL_LOCAL_HOST'),
                                         port = os.environ.get('POSTGRESQL_LOCAL_PORT'),
                                         database = os.environ.get('POSTGRESQL_LOCAL_DATABASE_MARKET_TEXT') )
+  """
   cursor = connection_postgres.cursor()
   return connection_postgres, cursor

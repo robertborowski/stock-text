8992e3291c0ce08d5873efeb5c4f3957211e2b8e
robert borowski <rborowski141@gmail.com>
2021-02-26 06:49:28 -0500

    confirm email link test

--- backend/login_and_create_account/create_confirm_token.py
+++ backend/login_and_create_account/create_confirm_token.py
@@ -9,6 +9,7 @@ def create_confirm_token_function(user_email):
   serializer_instance = URLSafeTimedSerializer(os.environ.get('URL_SAFE_SERIALIZER_SECRET_KEY'))
   string_to_salt = 'confirmemail'.encode("utf-8")
   print('- - - - - - -')
+  print(type(serializer_instance))
   print(type(string_to_salt))
   print('- - - - - - -')
   token = serializer_instance.dumps(user_email, salt=string_to_salt)

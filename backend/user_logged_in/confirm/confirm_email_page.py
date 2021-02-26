90d6f3d99757f389663958309317554b3cddd087
robert borowski <rborowski141@gmail.com>
2021-02-26 07:15:01 -0500

    confirm email link test

--- backend/user_logged_in/confirm/confirm_email_page.py
+++ backend/user_logged_in/confirm/confirm_email_page.py
@@ -3,7 +3,7 @@ from itsdangerous import URLSafeTimedSerializer
 import os
 
 confirm_email_page = Blueprint("confirm_email_page", __name__, static_folder="static", template_folder="templates")
-@confirm_email_page.route("/confirm/email/<session['confirm_email_token']>", methods=["POST", "GET"])
+@confirm_email_page.route("/confirm/<session['confirm_email_token']>", methods=["POST", "GET"])
 def confirm_email_page_function():
   """
   Returns: confirms email token link
@@ -12,12 +12,13 @@ def confirm_email_page_function():
   print(session['confirm_email_token'])
   print('- - - - - - - - -')
 
-  serializer_instance = URLSafeTimedSerializer(os.environ.get('URL_SAFE_SERIALIZER_SECRET_KEY'))#.encode("utf-8"))
+  serializer_instance = URLSafeTimedSerializer(os.environ.get('URL_SAFE_SERIALIZER_SECRET_KEY'))
   string_to_salt = "confirmemail".encode("utf-8")
   try:
     user_email_confirming = serializer_instance.loads(session['confirm_email_token'], salt=string_to_salt, max_age=3600)
   except:
     print('the token is expired!')
-    return '<h1>the token is expired!</h1>'
+    return render_template('templates_user_logged_in/confirmed_email_page.html',
+                          error_message_to_html = 'NOPE!')
   return render_template('templates_user_logged_in/confirmed_email_page.html',
                           user_email_confirming_to_html = user_email_confirming)

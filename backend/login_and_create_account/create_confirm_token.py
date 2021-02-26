95f7334a99c2f97e38381ee8fec3bc6448050bde
robert borowski <rborowski141@gmail.com>
2021-02-26 07:35:03 -0500

    confirm email link test

--- backend/login_and_create_account/create_confirm_token.py
+++ backend/login_and_create_account/create_confirm_token.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request, session, Blueprint
 from itsdangerous import URLSafeTimedSerializer
 import os
+#==================================
+from backend.user_logged_in.confirm.confirm_email_page import confirm_email_page
+#==================================
 
 def create_confirm_token_function(user_email):
   """
@@ -10,5 +13,12 @@ def create_confirm_token_function(user_email):
   string_to_salt = 'confirmemail'.encode("utf-8")
   token = serializer_instance.dumps(user_email, salt=string_to_salt)
   session['confirm_email_token'] = token
-  
+  #==================================
+  # Flask constructor
+  app = Flask(__name__)
+  # To use a session, there has to be a secret key. The string should be something difficult to guess
+  app.secret_key = os.urandom(64)
+  app.register_blueprint(confirm_email_page, url_prefix="")
+  #==================================
+
   return token

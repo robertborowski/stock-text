806e7b88f1b43388456ea224ed98441769fc2129
robert borowski <rborowski141@gmail.com>
2021-02-27 16:43:43 -0500

    confirm email link test

--- backend/login_and_create_account/creating_account_to_postgres.py
+++ backend/login_and_create_account/creating_account_to_postgres.py
@@ -14,6 +14,7 @@ from backend.utils.set_session_variables_to_none_logout import set_session_varia
 from backend.utils.constant_run.twilio.send_email_confirm_account import send_email_confirm_account_function
 from backend.login_and_create_account.create_confirm_token import create_confirm_token_function
 #=================================
+from backend.user_logged_in.confirm.confirm_email_page import confirm_email_page
 from backend.user_logged_in.confirm.confirm_email_page import confirm_email_page_function
 #=================================
 
@@ -55,7 +56,7 @@ def creating_account_to_postgres_function():
     confirm_email_token = create_confirm_token_function(user_email_from_html_form_sanitized)
     #==================================
     session['confirm_email_token_session'] = confirm_email_token
-    url_for(confirm_email_page_function, variable_passed_in = session['confirm_email_token_session'])
+    url_for('confirm_email_page', variable_passed_in = session['confirm_email_token_session'])
     #==================================
     send_email_confirm_account_function(user_email_from_html_form_sanitized, user_first_name_from_html_form_sanitized, confirm_email_token)
     output_message = 'Please confirm email (link sent to email) and phone number (link sent to phone number)'

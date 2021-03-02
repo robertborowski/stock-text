16e21b31e649589e02a22183106dd5b57d6a4d18
robert borowski <rborowski141@gmail.com>
2021-03-01 21:11:05 -0500

    session perm

--- backend/login_and_create_account/loginpage.py
+++ backend/login_and_create_account/loginpage.py
@@ -6,4 +6,7 @@ def index_function():
   """
   Returns: Renders the login page
   """
-  return render_template('templates_login_and_create_account/index.html')
+  if session['logged_in_user_email'] != 'none':
+    return render_template('templates_user_logged_in/loggedin_home_page.html')
+  else:
+    return render_template('templates_login_and_create_account/index.html')

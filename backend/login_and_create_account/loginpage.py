13f1ce2dc4374da1e63ebc136c159ea361af5a43
robert borowski <rborowski141@gmail.com>
2021-03-02 22:11:52 -0500

    working on domain

--- backend/login_and_create_account/loginpage.py
+++ backend/login_and_create_account/loginpage.py
@@ -5,9 +5,11 @@ from urllib.parse import urlparse, urlunparse
 from backend.utils.app_before_setup_folder.app_before_setup import app_before_setup_function
 
 loginpage = Blueprint("loginpage", __name__, static_folder="static", template_folder="templates")
+
 @loginpage.before_request
 def before_request_function():
   app_before_setup_function()
+
 @loginpage.route("/")
 def index_function():
   """

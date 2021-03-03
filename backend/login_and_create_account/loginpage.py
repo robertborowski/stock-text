e049dc52683f206c13e68ee91c2a96a6e4abfed9
robert borowski <rborowski141@gmail.com>
2021-03-02 22:15:30 -0500

    working on domain

--- backend/login_and_create_account/loginpage.py
+++ backend/login_and_create_account/loginpage.py
@@ -1,15 +1,16 @@
 from flask import render_template, Blueprint, session, url_for, redirect, request
 from backend.utils.set_session_variables_to_none_logout import set_session_variables_to_none_logout_function
 from backend.user_logged_in.home.homepage import homepage
-from urllib.parse import urlparse, urlunparse
 from backend.utils.app_before_setup_folder.app_before_setup import app_before_setup_function
 
 loginpage = Blueprint("loginpage", __name__, static_folder="static", template_folder="templates")
 
+# Before loading app URL
 @loginpage.before_request
 def before_request_function():
   app_before_setup_function()
 
+# Load app URL
 @loginpage.route("/")
 def index_function():
   """

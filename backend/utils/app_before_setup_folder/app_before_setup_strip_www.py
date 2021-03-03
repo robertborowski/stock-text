3bac395d067dc41564ba0a80288c54b0d510fa13
robert borowski <rborowski141@gmail.com>
2021-03-03 06:39:40 -0500

    working on domain

--- backend/utils/app_before_setup_folder/app_before_setup_strip_www.py
+++ backend/utils/app_before_setup_folder/app_before_setup_strip_www.py
@@ -10,4 +10,7 @@ def app_before_setup_strip_www_function(extension_url):
   if urlparts.netloc == url_to_search:
     urlparts_list = list(urlparts)
     urlparts_list[1] = 'symbolnews.com' + extension_url
-    return redirect(urlunparse(urlparts_list), code=301)
+    print('- - - - - - - - - ')
+    print(urlparts_list)
+    print('- - - - - - - - - ')
+    return redirect(urlunparse(urlparts_list), code=301) 

89b29a7ff80b310e6b6da8212281c36910533aa7
robert borowski <rborowski141@gmail.com>
2021-03-03 21:01:32 -0500

    working on domain

--- backend/login_and_create_account/create_account_render_page.py
+++ backend/login_and_create_account/create_account_render_page.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, redirect, request
+from flask import render_template, Blueprint, redirect, request, session
 from backend.utils.app_before_setup.check_if_url_www import check_if_url_www_function
 from backend.utils.app_before_setup.remove_www_from_domain import remove_www_from_domain_function
 
@@ -15,4 +15,11 @@ def create_account_function():
     # Redirect page to non-www
     return redirect(new_url, code=301)
 
-  return render_template('templates_login_and_create_account/create_account.html')
+  # # If session info found
+  if session and 'logged_in_user_email' in session and session.get('logged_in_user_email') != 'none':
+    session.permanent = True
+    return redirect("https://symbolnews.com/home", code=301)
+  
+  # If no session info found
+  else:
+    return render_template('templates_login_and_create_account/create_account.html')

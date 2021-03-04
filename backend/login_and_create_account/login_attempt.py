03977d8413eff779b2cac8939c0af2112097a174
robert borowski <rborowski141@gmail.com>
2021-03-04 07:59:55 -0500

    working on domain

--- backend/login_and_create_account/login_attempt.py
+++ backend/login_and_create_account/login_attempt.py
@@ -45,7 +45,7 @@ def login_attempt_function():
     if session.get('form_data_login_attempt_password') == None:
       session['form_data_login_attempt_password'] = request.form.get("psw")
       if session.get('form_data_login_attempt_password') == None:
-        session['form_data_login_attempt_password'] = "temp_placeholder_password_123$"
+        session['form_data_login_attempt_password'] = "tempPlaceholderPassword123$"
 
     # Sanitize user inputs
     user_email_from_html_form_sanitized = sanitize_email_input_create_account_function(session.get('form_data_login_attempt_email'))

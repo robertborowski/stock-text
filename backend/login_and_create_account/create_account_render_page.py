09d16f2a6a13eb669b0529e7778fbf12dc6728bb
robert borowski <rborowski141@gmail.com>
2021-03-03 20:58:37 -0500

    working on domain

--- backend/login_and_create_account/create_account_render_page.py
+++ backend/login_and_create_account/create_account_render_page.py
@@ -1,4 +1,6 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, request
+from backend.utils.app_before_setup.check_if_url_www import check_if_url_www_function
+from backend.utils.app_before_setup.remove_www_from_domain import remove_www_from_domain_function
 
 create_account_render_page = Blueprint("create_account_render_page", __name__, static_folder="static", template_folder="templates")
 @create_account_render_page.route("/create_account")
@@ -6,4 +8,11 @@ def create_account_function():
   """
   Returns: Renders the create account page
   """
+  # Domain Check #1 - Does it start with www.
+  www_start = check_if_url_www_function(request.url)
+  if www_start:
+    new_url = remove_www_from_domain_function(request.url)
+    # Redirect page to non-www
+    return redirect(new_url, code=301)
+
   return render_template('templates_login_and_create_account/create_account.html')
--- backend/login_and_create_account/loginpage.py
+++ backend/login_and_create_account/loginpage.py
@@ -3,16 +3,17 @@ from backend.utils.set_session_variables_to_none_logout import set_session_varia
 from backend.user_logged_in.home.homepage import homepage
 
 loginpage = Blueprint("loginpage", __name__, static_folder="static", template_folder="templates")
-
-# Load app URL
 @loginpage.route("/")
 def index_function():
   """
   Returns: Renders the login page
   """
+  # # If session info found
   if session and 'logged_in_user_email' in session and session.get('logged_in_user_email') != 'none':
     session.permanent = True
     return redirect("https://symbolnews.com/home", code=301)
+  
+  # If no session info found
   else:
-    #set_session_variables_to_none_logout_function()
+    set_session_variables_to_none_logout_function()
     return render_template('templates_login_and_create_account/index.html')

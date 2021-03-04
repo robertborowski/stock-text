95723b029ac981e1337407ec24c1d97f66f0825f
robert borowski <rborowski141@gmail.com>
2021-03-04 07:51:10 -0500

    working on domain

--- backend/login_and_create_account/forgot_password_send_token_to_email.py
+++ backend/login_and_create_account/forgot_password_send_token_to_email.py
@@ -40,7 +40,7 @@ def forgot_password_send_token_to_email_function():
     if session.get('form_data_forgot_password_email') == None:
       session['form_data_forgot_password_email'] = request.form.get("email")
       if session.get('form_data_forgot_password_email') == None:
-        session['form_data_forgot_password_email'] = "temp_placeholder_email"
+        session['form_data_forgot_password_email'] = "temp_placeholder_email@a.com"
     
     # Sanatize the user email
     user_email_from_html_form_sanitized = sanitize_email_input_create_account_function(session.get('form_data_forgot_password_email'))

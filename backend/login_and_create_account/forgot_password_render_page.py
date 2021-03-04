ea337c29b9bc879a72e39e3e7ad4b641f2cf915d
robert borowski <rborowski141@gmail.com>
2021-03-03 21:48:30 -0500

    working on domain

--- backend/login_and_create_account/forgot_password_render_page.py
+++ backend/login_and_create_account/forgot_password_render_page.py
@@ -3,11 +3,9 @@ from backend.utils.app_before_setup.check_if_url_www import check_if_url_www_fun
 from backend.utils.app_before_setup.remove_www_from_domain import remove_www_from_domain_function
 
 forgot_password_render_page = Blueprint("forgot_password_render_page", __name__, static_folder="static", template_folder="templates")
-@forgot_password_render_page.route("/forgot_password")
-def forgot_password_render_page_function():
-  """
-  Returns: Renders the create account page
-  """
+
+@forgot_password_render_page.before_request
+def before_request():
   # Domain Check #1 - Does it start with www.
   www_start = check_if_url_www_function(request.url)
   if www_start:
@@ -15,7 +13,12 @@ def forgot_password_render_page_function():
     # Redirect page to non-www
     return redirect(new_url, code=301)
 
-  # # If session info found
+@forgot_password_render_page.route("/forgot_password")
+def forgot_password_render_page_function():
+  """
+  Returns: Renders the create account page
+  """
+  # If session info found
   if session and 'logged_in_user_email' in session and session.get('logged_in_user_email') != 'none':
     session.permanent = True
     return redirect("https://symbolnews.com/home", code=301)
--- backend/login_and_create_account/forgot_password_send_token_to_email.py
+++ backend/login_and_create_account/forgot_password_send_token_to_email.py
@@ -14,11 +14,9 @@ from backend.utils.app_before_setup.check_if_url_www import check_if_url_www_fun
 from backend.utils.app_before_setup.remove_www_from_domain import remove_www_from_domain_function
 
 forgot_password_send_token_to_email = Blueprint("forgot_password_send_token_to_email", __name__, static_folder="static", template_folder="templates")
-@forgot_password_send_token_to_email.route("/forgot_password/send_link_attempt", methods=["POST", "GET"])
-def forgot_password_send_token_to_email_function():
-  """
-  Returns: login attempt on index/login page
-  """
+
+@forgot_password_send_token_to_email.before_request
+def before_request():
   # Domain Check #1 - Does it start with www.
   www_start = check_if_url_www_function(request.url)
   if www_start:
@@ -26,6 +24,11 @@ def forgot_password_send_token_to_email_function():
     # Redirect page to non-www
     return redirect(new_url, code=301)
 
+@forgot_password_send_token_to_email.route("/forgot_password/send_link_attempt", methods=["POST", "GET"])
+def forgot_password_send_token_to_email_function():
+  """
+  Returns: login attempt on index/login page
+  """
   # Sanatize the user email
   user_email_from_html_form_sanitized = sanitize_email_input_create_account_function(request.form.get("email"))
   

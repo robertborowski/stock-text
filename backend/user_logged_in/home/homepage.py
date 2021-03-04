2ca4d58923fc9aa3555ae9d7eac11ce96f9a883d
robert borowski <rborowski141@gmail.com>
2021-03-03 20:48:16 -0500

    working on domain

--- backend/user_logged_in/home/homepage.py
+++ backend/user_logged_in/home/homepage.py
@@ -3,28 +3,24 @@ from backend.db.connect_to_database import connect_to_postgres_function
 from backend.db.queries.select_queries.select_user_tracking_list import select_user_tracking_list_function
 from backend.db.close_connection_cursor_to_database import close_connection_cursor_to_database_function
 from backend.utils.set_session_variables_to_none_logout import set_session_variables_to_none_logout_function
-#==========================
-from urllib.parse import urlparse, urlunparse
 from backend.utils.app_before_setup.remove_www_from_domain import remove_www_from_domain_function
 from backend.utils.app_before_setup.check_if_url_www import check_if_url_www_function
-#==========================
 
 homepage = Blueprint("homepage", __name__, static_folder="static", template_folder="templates")
-
 @homepage.route("/home", methods=["POST", "GET"])
 def logged_in_home_page_function():
   """
   Returns: homepage front end template with user symbol tracking list
   """
-  #=======================
   # Domain Check #1 - Does it start with www.
   www_start = check_if_url_www_function(request.url)
   if www_start:
     new_url = remove_www_from_domain_function(request.url)
+    # Redirect page to non-www
     return redirect(new_url, code=301)
-  #=======================
-  #if session['logged_in_user_email'] != 'none':
-  if session and 'logged_in_user_email' in session and session.get('logged_in_user_email') != 'none':
+
+  if session['logged_in_user_email'] != 'none':
+  #if session and 'logged_in_user_email' in session and session.get('logged_in_user_email') != 'none':
     connection_postgres, cursor = connect_to_postgres_function()
     symbol_tracking_list = select_user_tracking_list_function(connection_postgres, cursor, session['logged_in_user_uuid'])
     close_connection_cursor_to_database_function(connection_postgres, cursor)
@@ -35,8 +31,5 @@ def logged_in_home_page_function():
                             user_phone_number_from_session_to_html = session['logged_in_user_phone_number'],
                             symbol_tracking_list_from_python_to_html = symbol_tracking_list)
   else:
-    print('=============================')
-    print(session.get('logged_in_user_email'))
-    print('=============================')
     set_session_variables_to_none_logout_function()
     return render_template('templates_login_and_create_account/index.html')

0b0342ea09fa452ae9a063fd58d9d38953c5a776
robert borowski <rborowski141@gmail.com>
2021-03-06 09:08:27 -0500

    working on domain

--- backend/login_and_create_account/login/loginpage.py
+++ backend/login_and_create_account/login/loginpage.py
@@ -1,8 +1,20 @@
 from flask import render_template, Blueprint, session, url_for, redirect, request
 from backend.utils.set_session_variables_to_none_logout import set_session_variables_to_none_logout_function
 from backend.user_logged_in.home.homepage import homepage
+from backend.utils.app_before_setup.check_if_url_www import check_if_url_www_function
+from backend.utils.app_before_setup.remove_www_from_domain import remove_www_from_domain_function
 
 loginpage = Blueprint("loginpage", __name__, static_folder="static", template_folder="templates")
+
+@loginpage.before_request
+def before_request():
+  # Domain Check #1 - Does it start with www.
+  www_start = check_if_url_www_function(request.url)
+  if www_start:
+    new_url = remove_www_from_domain_function(request.url)
+    # Redirect page to non-www
+    return redirect(new_url, code=301)
+
 @loginpage.route("/")
 def index_function():
   """

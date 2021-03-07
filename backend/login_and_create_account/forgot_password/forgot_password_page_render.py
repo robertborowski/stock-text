e5ef0133feb2ba16d732990a42425c736bb8c107
robert borowski <rborowski141@gmail.com>
2021-03-07 10:25:54 -0500

    rewriting the render page layouts and ordering

--- backend/login_and_create_account/forgot_password/forgot_password_page_render.py
+++ backend/login_and_create_account/forgot_password/forgot_password_page_render.py
@@ -2,25 +2,19 @@ from flask import render_template, Blueprint, redirect, request, session
 from backend.utils.app_before_setup.check_if_url_www import check_if_url_www_function
 from backend.utils.app_before_setup.remove_www_from_domain import remove_www_from_domain_function
 
-forgot_password_render_page = Blueprint("forgot_password_render_page", __name__, static_folder="static", template_folder="templates")
+forgot_password_page_render = Blueprint("forgot_password_page_render", __name__, static_folder="static", template_folder="templates")
 
-@forgot_password_render_page.before_request
+@forgot_password_page_render.before_request
 def before_request():
-  # Domain Check #1 - Does it start with www.
+  """Returns: The domain should work with both www and non-www domain"""
   www_start = check_if_url_www_function(request.url)
   if www_start:
     new_url = remove_www_from_domain_function(request.url)
-    # Redirect page to non-www
     return redirect(new_url, code=301)
 
-@forgot_password_render_page.route("/forgot_password")
-def forgot_password_render_page_function():
+@forgot_password_page_render.route("/forgot_password")
+def forgot_password_page_render_function():
   """
   Returns: Renders the create account page
   """
-  # If session info found
-  if session and 'logged_in_user_email' in session and session.get('logged_in_user_email') != 'none':
-    session.permanent = True
-    return redirect("https://symbolnews.com/home", code=301)
-
   return render_template('templates_login_and_create_account/forgot_password_page.html')
--- index.py
+++ index.py
@@ -6,8 +6,8 @@ import datetime
 from backend.login_and_create_account.login.login_page_render import login_page_render    #<------- Updated this
 from backend.login_and_create_account.login.login_attempt import login_attempt    #<------- Updated this
 from backend.login_and_create_account.create_account.create_account_page_render import create_account_page_render    #<------- Updated this
-from backend.login_and_create_account.create_account.creating_account_to_postgres import creating_account_to_postgres
-from backend.login_and_create_account.forgot_password.forgot_password_render_page import forgot_password_render_page
+from backend.login_and_create_account.create_account.creating_account_to_postgres import creating_account_to_postgres    #<------- Updated this
+from backend.login_and_create_account.forgot_password.forgot_password_page_render import forgot_password_page_render    #<------- Updated this
 from backend.login_and_create_account.forgot_password.forgot_password_send_token_to_email import forgot_password_send_token_to_email
 from backend.login_and_create_account.forgot_password.set_new_password import set_new_password
 from backend.login_and_create_account.forgot_password.confirm_new_password_set import confirm_new_password_set
@@ -44,8 +44,8 @@ app.permanent_session_lifetime = datetime.timedelta(days=365)
 app.register_blueprint(login_page_render, url_prefix="")    #<------- Updated this
 app.register_blueprint(login_attempt, url_prefix="")    #<------- Updated this
 app.register_blueprint(create_account_page_render, url_prefix="")    #<------- Updated this
-app.register_blueprint(creating_account_to_postgres, url_prefix="")
-app.register_blueprint(forgot_password_render_page, url_prefix="")
+app.register_blueprint(creating_account_to_postgres, url_prefix="")    #<------- Updated this
+app.register_blueprint(forgot_password_page_render, url_prefix="")    #<------- Updated this
 app.register_blueprint(forgot_password_send_token_to_email, url_prefix="")
 app.register_blueprint(set_new_password, url_prefix="")
 app.register_blueprint(confirm_new_password_set, url_prefix="")

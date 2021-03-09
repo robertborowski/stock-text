d99dfc1ed4a7e12c98f716da7e0e54112dea0a79
robert borowski <rborowski141@gmail.com>
2021-03-08 20:02:23 -0500

    rewriting the render page layouts and ordering

--- backend/utils/sanitize_user_inputs/sanitize_email_input_create_account.py
+++ backend/utils/sanitize_user_inputs/sanitize_email_input_create_account.py
@@ -4,7 +4,7 @@ def sanitize_email_input_create_account_function(email_not_sanitized):
   """
   Returns: Checks if the user inputs are valid/sanitized
   """
-  pattern = re.compile("^[a-z0-9._-]{1,50}@[a-z0-9._-]{1,30}\.[a-z]{2,3}$")
+  pattern = re.compile("^[A-Za-z0-9._-]{1,50}@[A-Za-z0-9._-]{1,30}\.[A-Za-z]{2,3}$")
   if pattern.match(email_not_sanitized):
     email_sanitized = email_not_sanitized
     return email_sanitized

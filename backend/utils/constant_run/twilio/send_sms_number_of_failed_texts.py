55239477b7c279e26fa2b2d4bfdd1412beadd6ae
robert borowski <rborowski141@gmail.com>
2021-03-18 10:22:44 -0400

    testing text out

--- backend/utils/constant_run/twilio/send_sms_number_of_failed_texts.py
+++ backend/utils/constant_run/twilio/send_sms_number_of_failed_texts.py
@@ -10,12 +10,15 @@ def send_sms_number_of_failed_texts_function(connection_postgres, cursor, num_te
   """
   Returns: Send update text to user
   """
-  
+  phone_number = os.environ.get('PERSONAL_PHONE_NUMBER')
+  print(phone_number)
+  print(os.environ.get('TWILIO_PHONE_NUMBER'))
+
   account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
   auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
   client = Client(account_sid, auth_token)
 
   message = client.messages.create(body=str(num_texts_failed_to_send) + ' texts failed to send today.',
                                   from_=os.environ.get('TWILIO_PHONE_NUMBER'),
-                                  to=os.environ.get('MY_PHONE_NUMBER'))
+                                  to=phone_number)
   print(message.sid)

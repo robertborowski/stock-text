48c3388be73e940235bcbf3a8db6ce7811c86635
robert borowski <rborowski141@gmail.com>
2021-03-26 07:13:29 -0400

    fixing bugs for input symbol and threshold dashboard

--- backend/utils/sanitize_user_inputs/sanitize_symbol_input.py
+++ backend/utils/sanitize_user_inputs/sanitize_symbol_input.py
@@ -5,7 +5,12 @@ def sanitize_symbol_input_function(symbol_not_sanitized):
   Returns: Checks if the user inputs are valid/sanitized
   """
   symbol_not_sanitized = symbol_not_sanitized.upper()
-  symbol_not_sanitized.replace('.', '-')
+  symbol_not_sanitized = symbol_not_sanitized.replace('.', '-')
+  print('-----------------------------------')
+  print('-------------------')
+  print(symbol_not_sanitized)
+  print('-------------------')
+  print('-----------------------------------')
 
   pattern = re.compile("^[A-Z-]{1,6}$")
   if pattern.match(symbol_not_sanitized):

4d2ea7757ebd5b6566e1561a976138e0e2370cad
robert borowski <rborowski141@gmail.com>
2021-03-26 07:16:53 -0400

    update sanitize input for dashboard stock

--- backend/utils/sanitize_user_inputs/sanitize_symbol_input.py
+++ backend/utils/sanitize_user_inputs/sanitize_symbol_input.py
@@ -6,11 +6,6 @@ def sanitize_symbol_input_function(symbol_not_sanitized):
   """
   symbol_not_sanitized = symbol_not_sanitized.upper()
   symbol_not_sanitized = symbol_not_sanitized.replace('.', '-')
-  print('-----------------------------------')
-  print('-------------------')
-  print(symbol_not_sanitized)
-  print('-------------------')
-  print('-----------------------------------')
 
   pattern = re.compile("^[A-Z-]{1,6}$")
   if pattern.match(symbol_not_sanitized):
@@ -18,12 +13,3 @@ def sanitize_symbol_input_function(symbol_not_sanitized):
     return symbol_sanitized
   else:
     return 'none'
-
-  """
-  if (len(symbol_not_sanitized) < 1 or len(symbol_not_sanitized) > 6): #or symbol_not_sanitized.isalpha() == False:
-    for i in symbol_not_sanitized:
-
-    return 'none'
-  symbol_sanitized = symbol_not_sanitized
-  return symbol_sanitized
-  """

f680043d341ef14a85c71d86f6f95731f59ab7ca
robert borowski <rborowski141@gmail.com>
2021-03-27 06:56:20 -0400

    update sanitize input for dashboard stock

--- backend/db/queries/select_queries/select_job_get_company_short_name.py
+++ backend/db/queries/select_queries/select_job_get_company_short_name.py
@@ -4,8 +4,11 @@ from psycopg2 import Error
 def select_job_get_company_short_name_function(connection_postgres, cursor, job_name_to_search):
   """Returns: Pulls all the symbols and percentages that the user is tracking"""
   try:
-    cursor.execute("SELECT stored_item FROM jobs_queues_table WHERE job_queue_name=%s", [job_name_to_search])
+    cursor.execute("""SELECT stored_item FROM jobs_queues_table WHERE job_queue_name=%s""", [job_name_to_search])
     result_list = cursor.fetchall()
+    print('- - - -')
+    print(result_list)
+    print('- - - -')
     return result_list
   except (Exception, psycopg2.Error) as error:
     if(connection_postgres):

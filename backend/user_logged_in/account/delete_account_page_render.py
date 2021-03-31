8f7f8e7445481c3bc576069f5deaa785f8fa1463
robert borowski <rborowski141@gmail.com>
2021-03-31 07:37:55 -0400

    update the dashboard and account pages

--- backend/user_logged_in/account/delete_account_page_render.py
+++ backend/user_logged_in/account/delete_account_page_render.py
@@ -2,6 +2,7 @@ from flask import render_template, Blueprint, session, redirect, request
 from backend.utils.set_session_variables_to_none_logout import set_session_variables_to_none_logout_function
 from backend.utils.app_before_setup.check_if_url_www import check_if_url_www_function
 from backend.utils.app_before_setup.remove_www_from_domain import remove_www_from_domain_function
+from backend.utils.create_uuid import create_uuid_function
 
 delete_account_page_render = Blueprint("delete_account_page_render", __name__, static_folder="static", template_folder="templates")
 
@@ -15,8 +16,11 @@ def before_request():
 
 @delete_account_page_render.route("/account/delete", methods=["POST", "GET"])
 def delete_account_page_render_function():
+  # Need to create a css unique key so that cache busting can be done
+  css_cache_busting_variable = create_uuid_function('css_')
+
   if session and 'logged_in_user_email' in session and session.get('logged_in_user_email') != 'none':
-    return render_template('templates_user_logged_in/account_delete.html')
+    return render_template('templates_user_logged_in/account_delete.html', css_cache_busting_variable_to_html = css_cache_busting_variable)
   else:
     set_session_variables_to_none_logout_function()
     return redirect("https://symbolnews.com/", code=301)

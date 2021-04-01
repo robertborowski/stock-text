78da884c159521edd45cc915923c107d50ce1a01
robert borowski <rborowski141@gmail.com>
2021-04-01 07:53:17 -0400

    when creating account check for phone number duplicates as well as email

--- backend/db/queries/select_queries/select_login_information_table_query_phone_number.py
+++ backend/db/queries/select_queries/select_login_information_table_query_phone_number.py
@@ -7,7 +7,7 @@ def select_login_information_table_query_phone_number_function(connection_postgr
     cursor.execute("SELECT phone_number FROM login_information_table WHERE phone_number=%s", [phone_to_search])
     result_row = cursor.fetchone()
     result_email = result_row[0]
-    if result_email == email_to_search:
+    if result_email == phone_to_search:
       phone_exists = 'Account already exists'
       return phone_exists
   except (Exception, psycopg2.Error) as error:

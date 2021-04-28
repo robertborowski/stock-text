d4a5a4cad8b4de190d6dccf4e8870b4b0803c332
robert borowski <rborowski141@gmail.com>
2021-04-27 20:01:20 -0400

    testing jobs folder

---
+++ jobs/job_test.py
@@ -0,0 +1,17 @@
+def job_test_function():
+  print(' - - - - - - - - - - - - - - - - - - - - - ')
+  print(' - - - - - - - - - - - - - - - -')
+  print(' - - - - - - - - - - ')
+  print(' - - - - - - ')
+  print(' - - -')
+  print('JOB RAN FROM JOB FOLDER!!!!!!!!!!!')
+  print(' - - -')
+  print(' - - - - - - ')
+  print(' - - - - - - - - - - ')
+  print(' - - - - - - - - - - - - - - - -')
+  print(' - - - - - - - - - - - - - - - - - - - - - ')
+
+
+# Run the main program
+if __name__ == "__main__":
+  job_test_function()
